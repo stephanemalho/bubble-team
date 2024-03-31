6ba844a05bc2e5a5113ef1116afe1559740546c8
stephane.malho <stepahen.malho@gmail.com>
2024-03-31 18:52:13 +0200

    F04: OrderPage >  Navbar > layout > create left & right side cpnt

--- src/components/pages/order/Navbar.tsx
+++ src/components/pages/order/Navbar.tsx
@@ -6,10 +6,15 @@ export default function Navbar() {
 
   return (
     <NavbarStyled className="navbar">
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Retour à la page d'accueil</button>
-      </Link>
+      <div className="left-side">
+        <h1>LOGO</h1>
+      </div>
+      <div className="right-side">
+        <h1>Bonjour {username}</h1>
+        <Link to="/">
+          <button>Retour à la page d'accueil</button>
+        </Link>
+      </div>
     </NavbarStyled>
   )
 }
@@ -17,4 +22,15 @@ export default function Navbar() {
 const NavbarStyled = styled.nav`
       background-color: blue;
       height: 10vh;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .left-side {
+                background-color: pink;
+      }
+
+      .right-side {
+        background-color: yellow;
+      }
 `;
