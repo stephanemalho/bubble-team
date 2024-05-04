946cc7f560f7a4a02a966c9d8a7749fb69b62dda
stephane.malho <stepahen.malho@gmail.com>
2024-05-04 15:28:50 +0200

    ✨F04: Navbar > add reload on Logo container

--- src/components/pages/order/NavbarLeftSide.tsx
+++ src/components/pages/order/NavbarLeftSide.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 import Title from '../../ui/Title';
+import { refreshWindow } from '../../../utils/window';
 
 export default function NavbarLeftSide() {
   return (
-    <NavbarLeftSideStyled className="left-side">
+    <NavbarLeftSideStyled className="left-side" onClick={refreshWindow}>
       <Title size={2} rounded={3} />
     </NavbarLeftSideStyled>
   )
@@ -12,4 +13,5 @@ export default function NavbarLeftSide() {
 const NavbarLeftSideStyled = styled.div`
       display: flex;
       height: 100%; 
+      cursor: pointer;
 `;
