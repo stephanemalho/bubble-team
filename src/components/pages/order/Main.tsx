eab78b1ea50c923def7d1d8ead3ee2e76109d1c3
stephane.malho <stepahen.malho@gmail.com>
2024-05-12 12:12:07 +0200

    🚧 F05 : Order > add grid layout

--- src/components/pages/order/Main.tsx
+++ src/components/pages/order/Main.tsx
@@ -4,7 +4,8 @@ import { theme } from "../../../assets/theme";
 export default function Main() {
   return (
     <MainStyled className="main">
-      {""}
+      <div className="basket">Basket</div>
+      <div className="menu">Menu</div>
     </MainStyled>
   )
 }
@@ -13,4 +14,15 @@ const MainStyled = styled.main`
     background-color: ${theme.colors.background_white};
     box-shadow: 0px 8px 20px 8px rgba(142, 106, 146, 0.2) inset ;
     flex: 1;
+
+    display: grid;
+    grid-template-columns: 25% 1fr;
+
+    .basket {
+      background-color: pink;
+    }
+
+    .menu {
+      background-color: purple;
+    }
 `;
