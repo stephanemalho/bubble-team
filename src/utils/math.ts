9bcafdbb18810d1baa530527f5c52d7628d5abb2
stephane.malho <stepahen.malho@gmail.com>
2024-06-08 22:36:32 +0200

    ♻️ FO7 : utils > create formatIngredients

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -12,6 +12,14 @@ export function formatPrice(priceToFormat: number) {
   return formattedPrice
 }
 
+export const formatIngredients = (ingredients: string[]): string => {
+  if (ingredients.length === 0) return '';
+  if (ingredients.length === 1) return ingredients[0] + '.';
+  const allButLast = ingredients.slice(0, -1).join(', ');
+  const last = ingredients[ingredients.length - 1];
+  return `${allButLast} et ${last}.`;
+};
+
 export function replaceFrenchCommaWithDot(price: number | string) {
   if (typeof price === "string") price = parseFloat(price.replace(",", "."))
   return price
