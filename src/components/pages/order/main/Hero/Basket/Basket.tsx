1574cca1ec3cdffff82b674350b2fd16e3a2ce8f
stephane.malho <stepahen.malho@gmail.com>
2024-06-09 13:55:45 +0200

    💄  FO7 : Basket > add style to container

--- src/components/pages/order/main/Hero/Basket/Basket.tsx
+++ src/components/pages/order/main/Hero/Basket/Basket.tsx
@@ -1,11 +1,26 @@
+import styled from "styled-components";
+import { theme } from "../../../../../../assets/theme";
+
 type BasketProps = {
   isBasketOpen: boolean;
 };
 
 function Basket({ isBasketOpen }:BasketProps) {
   return (
-    <div className="basket">{isBasketOpen ? <div className="basketList">basket</div> : null}</div>
+    <BasketStyled>{isBasketOpen ? <div className="basketList">basket item</div> : <div className="basketList">basket empty</div>}</BasketStyled>
   )
 }
 
 export default Basket
+
+const BasketStyled = styled.div`
+ height: 90%;
+      margin: 10px;
+      border-radius: ${theme.borderRadius.extraRound};
+      background: rgba(255, 255, 255, 0.1);
+      box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+      position: relative;
+      backdrop-filter: blur(5px);
+      -webkit-backdrop-filter: blur(5px);
+      border: 1px solid rgba(255, 255, 255, 0.3);
+`;
